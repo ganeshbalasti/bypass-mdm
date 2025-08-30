05a7035e365d092f9285582d3476654bfc6a7b0a
Ganesh Balasti <gbmarketing.india@gmail.com>
2025-08-30 23:53:26 +0530

    Update bypass-mdm.sh

--- bypass-mdm.sh
+++ bypass-mdm.sh
@@ -51,6 +51,9 @@ select opt in "${options[@]}"; do
             echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
             echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
             echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 gdmf.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 acmdm.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 albert.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
             echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
             # Remove configuration profiles
